42f2824932ce5fe999470298767cba7f678428b2
prajjawaltiwari <prajjawaltiwari1811@gmail.com>
2021-09-19 23:25:18 +0530

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
 insert into Room(roomNo,type,price) values('001','standard','1200');
 insert into Room(roomNo,type,price) values('002','standard','1200');
 insert into Room(roomNo,type,price) values('003','standard','1200');
+insert into Room(roomNo,type,price) values('004','luxury','5000');
+insert into Room(roomNo,type,price) values('005','suite','8000');
