564c5462630c04502ccc3ca965a5d3c9ed889abe
prajjawaltiwari <prajjawaltiwari1811@gmail.com>
2021-09-19 23:14:03 +0530

    Delete src/main directory

--- src/main/java/com/Hotel/exception/TokenInvalidException.java
+++
@@ -1,7 +0,0 @@
-package com.Hotel.exception;
-
-public class TokenInvalidException extends Exception {
-	public TokenInvalidException(String msg) {
-		super(msg);
-	}
-}
--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into Room(roomNo,type,price) values('001','standard','1200');
-insert into Room(roomNo,type,price) values('002','standard','1200');
-insert into Room(roomNo,type,price) values('003','standard','1200');
